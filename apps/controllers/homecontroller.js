920db1f1f012254a5b45dd85239b946add5ce25d
Duong <93812104+duongkaja@users.noreply.github.com>
2023-03-20 08:51:19 +0700

    Delete apps directory

--- apps/controllers/homecontroller.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.use("/", function(req,res){
-    //res.json({"message": "this is home"});
-    res.render("home.ejs");
-});
-module.exports = router;
