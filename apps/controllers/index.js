455c5be2f07044527fbf561094613299684de08e
DUONG <duonghovo@gmail.com>
2023-04-02 14:14:12 +0700

    restart

--- apps/controllers/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-// router.use("/home", require(__dirname + "/homecontroller"));
-// router.use("/product", require(__dirname + "/productcontroller"));
-router.get("/", function(req,res){
-    // res.json({"message": "this is index page"});
-    res.render("index");
-});;
-module.exports = router;
