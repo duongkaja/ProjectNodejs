8754e3742b72b35e6ecdad8d743aeef59dd63c7c
Gia <lehoanggia85@gmail.com>
2023-04-06 00:05:44 +0700

    gia test

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
             </div>
             <h1>Traveling opens the door to creating <span className="hightlight"> memories </span></h1>
             <p>
-              Hello thầy
+              Hello thầy hiep 
             </p>
           </div>
         </Col>
