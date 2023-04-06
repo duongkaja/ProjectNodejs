b4bdb0942a1bf9b982a94eeb8788b64e9797adfa
Gia <lehoanggia85@gmail.com>
2023-04-06 13:38:56 +0700

    phút 40:43 chưa hiển thị lên web đc , ae lấy xún gắn đường dẫn dô nha

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,8 @@ import heroVideo from '../assets/images/hero-video.mp4'
 import Subtitle from './../shared/Subtitle';
 import worldImg from '../assets/images/world.png'
 
+import SearchBar from '../shared/SearchBar';
+
 const Home = () => {
   return <>
   
@@ -17,19 +19,45 @@ const Home = () => {
         <Col lg='6'>
           <div className="hero__content">
             <div className="hero__subtitle d-flex align-items-center">
-              <Subtitle subtitle={'Know Brfore You Go'}/>
+              <Subtitle subtitle={'Know Before You Go'}/>
               <img src={worldImg} alt="" />
             </div>
-            <h1>Traveling opens the door to creating <span className="hightlight"> memories </span></h1>
+            <h1>Traveling opens the door to creating <span className="highlight"> memories </span></h1>
             <p>
-              Hello thầy hiep 
+             khúc này gõ tiếng anh ko à lười quá (dô home.jsx sửa nha). 
             </p>
           </div>
         </Col>
+        <col lg='2'>
+          <div className="hero__img-box">
+            <img src ={heroImg} alt=""/> 
+          </div>
+        </col>
+        <col lg='2'>
+          <div className="hero__img-box mt-4">
+            <video src ={heroVideo} alt="" controls />
+          </div>
+        </col>
+        <col lg='2'>
+          <div className="hero__img-box mt-5"> 
+            <img src ={heroImg02} alt=""/>
+          </div>
+        </col>
+      <SearchBar />
       </Row>
     </Container>
   </section>
     {/* ========== hero section start ========== */}
+    <section>
+      <Container>
+        <row>
+          <col lg='3'>
+            <h5 className="services__subtitle"> what we service</h5>
+            <h2 className="services__title">we offer out best services</h2>
+          </col>
+        </row>
+      </Container>
+    </section>
   </>
 };
 
