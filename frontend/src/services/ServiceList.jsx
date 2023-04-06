0f9d7a350ea522cea9c66b2d452ff2716dbf3b6c
nhathoa11092001 <nhathoa11092001@example.com>
2023-04-06 21:58:54 +0700

    Xay dung fronend code file ServiceCard va TourCard 1:01:00

---
+++ frontend/src/services/ServiceList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ServiceCard from "./ServiceCard";
+import { Col } from "reactstrap";
+import weatherImg from '../assets/images/weather.png';
+import guideImg from '../assets/images/guide.png'
+import customizationImg from '../assets/images/customization.png';
+
+const servicesData =[ 
+    {
+      imgUrl: weatherImg,
+      title:"Calcultate Weather",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      imgUrl:guideImg,
+      title:"Best Tour Giude",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+
+    {
+      imgUrl:customizationImg,
+      title:"Customization",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+]
+const ServiceList = () => {
+  return(
+    <>
+    {
+    servicesData.map ((item, index) => (
+        <Col lg = "3"
+          key = {index}>
+          <ServiceCard item = {item} />
+        </Col>
+    ))}
+</>
+  ); 
+};
+
+export default ServiceList;
