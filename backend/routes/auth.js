a9c9b077c6d0f3a6c0fd42a25fec75571c9a94e1
DUONG <duonghovo@gmail.com>
2023-04-09 03:42:36 +0700

    duong backend book and review

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,6 +6,6 @@ const router = express.Router()
 
 router.post('/register', register)
 
-router.post('login', login)
+router.post('/login', login)
 
 export default router
---
+++ backend/routes/reviews.js
@@ -0,0 +1,11 @@
+
+import express from 'express'
+
+import { createReview } from '../controllers/reviewController.js';
+import { verifyUser } from '../utils/verifyToken.js';
+
+const router = express.Router();
+
+router.post('/:tourId',verifyUser, createReview);
+
+export default router;
