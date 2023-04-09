397bdb22c08ac1772553ea8dcd034967da6cc614
Gia <lehoanggia85@gmail.com>
2023-04-09 13:21:24 +0700

    link Url ở config.js xây dụng useFetch.js SearchResultList.jsx SearchBar.jsx 13:50

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import TourCard from '../../shared/TourCard';
-import TourData from '../../assets/data/tours';
+
 import { Col } from 'reactstrap';
 
-const FeaturedTourList = () => {
-  return( <>
+import useFetch from '../../hooks/useFetch';
+import { BASE_URL } from '../../utils/config';
 
-    {
-       TourData?.map(tour=>(
-            <Col lg= "3" className='mb-4' key={tour.id}>
+const FeaturedTourList = () => {
+
+    const {data: featuredTours, loading, error} = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
+
+    console.log(featuredTours);
+
+  return( 
+  <>
+  {
+    loading && <h4>
+      Loading......
+    </h4>
+  }
+  {
+    loading && <h4>
+      {error}
+    </h4> 
+  }
+ 
+    {!loading && !error && featuredTours?.map(tour=>(
+            <Col lg= "3" className='mb-4' key={tour._id}> 
                 <TourCard tour = { tour}/>
             </Col>
         ))}
